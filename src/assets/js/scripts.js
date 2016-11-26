c3b7b3f6b01ac07d62ba9f654ada1601199e8037
Unknown <hello@table.digital>
2016-11-26 18:12:44 +0000

    Social links hidden until scroll

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,3 +1,22 @@
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 // create social networking pop-ups
 (function() {
 	// link selector and pop-up window size
@@ -37,4 +56,49 @@
 		return !!popup;
 	}
 
+}());
+
+
+(function() {
+  var showOnScrollElems = document.querySelectorAll('.js-show-on-scroll');
+  var shown = false;
+  window.addEventListener('scroll', debounce(scrollHandler, 250));
+
+  for (var a = 0; a < showOnScrollElems.length; a++) {
+    addClass(showOnScrollElems[a], 'js-show-on-scroll--hidden');
+	}
+
+  setTimeout(function() {
+    for (var a = 0; a < showOnScrollElems.length; a++) {
+      addClass(showOnScrollElems[a], 'js-show-on-scroll--transition');
+    }
+  }, 500);
+
+  function addClass(elem, className) {
+    if (elem.classList) {
+      elem.classList.add(className);
+    }
+    else {
+      elem.className += ' ' + className;
+    }
+  }
+
+  function removeClass(elem, className) {
+    if (elem.classList) {
+      elem.classList.remove(className);
+    }
+    else {
+      elem.className = elem.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+  }
+
+  function scrollHandler(e) {
+    //console.log(shown, document.body.scrollTop);
+    if(!shown && document.body.scrollTop > 800) {
+      shown = true;
+      for (var a = 0; a < showOnScrollElems.length; a++) {
+        removeClass(showOnScrollElems[a], 'js-show-on-scroll--hidden');
+    	}
+    }
+  }
 }());
